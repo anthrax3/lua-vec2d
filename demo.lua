b15bf32e6a3f1b293eee63d28ca6ec2c41727fa0
piXelicidio <pxpublicity@gmail.com>
2017-03-24 19:09:56 -0400

    fixing vec.make develping demo

--- demo.lua
+++ demo.lua
@@ -1,1 +1,10 @@
-vec = require(vec2d)
+vec = require 'vec2d'
+
+print "Welcome vec2d demo:"
+
+-- creating a 2d vector
+local v = vec.make(10,10)
+print ("New vector V=("..v.x..','..v.y..')')
+
+
+-- setting 
--- vec2D.lua
+++ vec2D.lua
@@ -2,7 +2,7 @@
 --Author: Denys Almaral (denysalmaral.com) 
 --license: MIT
 
-[[
+--[[
   Purpose:
   Create a library of 2D vector functions to work with any table that contains x,y values.
   Be efficient, simple and fast, accept tables as parameters, avoid unnecessary validations.
@@ -10,8 +10,9 @@
 
 local vec = {}
 
-function vec.make(x,y)
-  return {x,y}
+--returns a 2d vector table {x,y} =  {xv, yv}
+function vec.make(xv, yv)
+  return {x=xv, y=yv}
 end
 
 function vec.set(v, x, y)
